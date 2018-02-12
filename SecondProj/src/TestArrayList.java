5234ced9be9565dae9cddda8c5cfa2ec84e802c4
farithbakri <farithbakri93@gmail.com>
2018-02-12 16:34:46 +0800

    Multi Dimension Array

--- SecondProj/src/TestArrayList.java
+++ SecondProj/src/TestArrayList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -13,7 +15,27 @@ public class TestArrayList {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<String> al = new ArrayList<>();
+		
+		
+		
+		for(String str:args) {
+			al.add(str);
+			
+		}
+		System.out.println("Array list is empty:" + al.isEmpty());
+		
+		//al.add(new Integer(10));
+		//al.add(Boolean.TRUE);
+		System.out.println("Original : " + al);
+		al.remove("Two");
+		System.out.println("Now:" + al);
+		al.set(1, "New");
+		
+		System.out.println("Now:" + al);
+		
+		System.out.println("Contains four:" + al.contains("four"));
+		
 	}
 
 }
